4e443f022ce6c3966c322d4de267deaea55fddb0
Adam D <kirbylover4000@gmail.com>
2013-05-25 00:17:43 +0930

    SBBulletinListCell accessory types

--- SpringBoard/SBBulletinListCell.h
+++ SpringBoard/SBBulletinListCell.h
@@ -1,3 +1,10 @@
+typedef enum {
+	SBBulletinListCellAccessoryStyleNone = 0,
+	SBBulletinListCellAccessoryStyleDot = 1
+} SBBulletinListCellAccessoryStyle;
+
 @interface SBBulletinListCell : UITableViewCell
+@property SBBulletinListCellAccessoryStyle bulletinAccessoryStyle;
+
 - (id)initWithLinenView:(id)linenView reuseIdentifier:(NSString *)reuseIdentifier;
 @end
