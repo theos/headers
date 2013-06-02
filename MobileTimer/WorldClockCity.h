e63228696a50c3001c026fe94abad6cbe9cb1a6e
Adam D <kirbylover4000@gmail.com>
2013-06-02 22:55:06 +0930

    remove unneeded properties in WorldClockCity

--- MobileTimer/WorldClockCity.h
+++ MobileTimer/WorldClockCity.h
@@ -2,8 +2,4 @@
 
 @interface WorldClockCity : NSObject
 @property (readonly) NSString *name;
-@property (readonly) NSString *countryName;
-@property (readonly) NSString *unlocalizedCityName;
-@property (readonly) NSString *unlocalizedCountryName;
-@property (readonly) NSString *countryCode;
 @end
