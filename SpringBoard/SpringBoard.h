865074b89a82f7d0ccff56cff031eeb410fe405d
Adam D <kirbylover4000@gmail.com>
2013-09-26 13:00:06 +0930

    SpringBoard: isLocked, _isDim, undim

--- SpringBoard/SpringBoard.h
+++ SpringBoard/SpringBoard.h
@@ -1,6 +1,9 @@
 @interface SpringBoard : UIApplication
 - (void)applicationOpenURL:(NSURL *)url publicURLsOnly:(BOOL)publicOnly;
 - (void)_relaunchSpringBoardNow;
+- (void)undim;
 
 @property (nonatomic, retain) UIWindow *window;
+@property (readonly) BOOL isLocked;
+@property (readonly) BOOL _isDim;
 @end
