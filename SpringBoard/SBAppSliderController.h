480b14b12230d1dd03953ce577ce6fde28ce8f23
Adam D <kirbylover4000@gmail.com>
2013-12-28 02:56:32 +1030

    add switchspring stuff (SBAppSliderController, SBAlertItem)

--- SpringBoard/SBAppSliderController.h
+++ SpringBoard/SBAppSliderController.h
@@ -1,7 +1,13 @@
-@interface SBAppSliderController : UIViewController
+#import "SBAppSliderScrollingViewDelegate.h"
 
-- (void)_layout;
+@class SBAppSliderScrollingViewController;
+
+@interface SBAppSliderController : UIViewController <SBAppSliderScrollingViewDelegate>
+
+- (NSString *)_displayIDAtIndex:(NSUInteger)index;
 
 @property (nonatomic, retain) UIScrollView *contentScrollView;
+@property (nonatomic, retain) NSMutableArray *applicationList;
+@property (nonatomic, retain) SBAppSliderScrollingViewController *pageController;
 
 @end
--- SpringBoardUI/SBAlertItem.h
+++ SpringBoardUI/SBAlertItem.h
@@ -1,5 +1,7 @@
 @interface SBAlertItem : NSObject
 
+- (void)dismiss;
+
 @property (nonatomic, retain) UIAlertView *alertSheet;
 
 @end
