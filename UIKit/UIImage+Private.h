182a3323d6f3acbfa6a85745d6597fda8686e9b3
Adam D <kirbylover4000@gmail.com>
2014-01-14 05:30:10 +1030

    ok apparently what i thought would work doesn't work

--- UIKit/UIImage+Private.h
+++ UIKit/UIImage+Private.h
@@ -1,10 +1,20 @@
 #import <IOSurface/IOSurface.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+UIImage *_UIImageWithName(NSString *name);
+#ifdef __cplusplus
+}
+#endif
+
 @interface UIImage (Private)
 
 + (UIImage *)kitImageNamed:(NSString *)name;
 + (UIImage *)imageNamed:(NSString *)name inBundle:(NSBundle *)bundle;
 
 - (instancetype)_initWithIOSurface:(IOSurfaceRef)surface scale:(CGFloat)scale orientation:(UIImageOrientation)orientation;
+
+@property CGFloat scale;
 
 @end
