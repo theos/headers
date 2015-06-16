127b41e518498e2db20b8ede37eac6779b4f10b7
Adam Demasi <kirbylover4000@gmail.com>
2015-06-16 21:58:34 +0930

    add +[BBAction action]

--- BulletinBoard/BBAction.h
+++ BulletinBoard/BBAction.h
@@ -1,5 +1,6 @@
 @interface BBAction : NSObject
 
++ (instancetype)action;
 + (instancetype)actionWithLaunchBundleID:(NSString *)bundleID callblock:(id)block;
 + (instancetype)actionWithLaunchURL:(NSURL *)url callblock:(id)block;
 
