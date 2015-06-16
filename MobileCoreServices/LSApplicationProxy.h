4df6a72e49dd1696fe5648a685d2ea5b27cdc864
Adam Demasi <kirbylover4000@gmail.com>
2015-06-16 21:59:01 +0930

    [MobileCoreServices] add LSOpenOperation; update LSApplicationProxy

--- MobileCoreServices/LSApplicationProxy.h
+++ MobileCoreServices/LSApplicationProxy.h
@@ -1,5 +1,8 @@
 @interface LSApplicationProxy : NSObject
 
++ (instancetype)applicationProxyForIdentifier:(NSString *)identifier;
+
 @property (nonatomic, retain) NSString *applicationIdentifier;
+@property (nonatomic, retain) NSString *localizedName;
 
 @end
