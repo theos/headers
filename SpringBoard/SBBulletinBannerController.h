ccb42442c24a66b9f856ca8418a35c630915228c
Adam Demasi <kirbylover4000@gmail.com>
2015-07-10 13:37:34 +0930

    [SpringBoard] add observer:addBulletin:… method for ios 8

--- SpringBoard/SBBulletinBannerController.h
+++ SpringBoard/SBBulletinBannerController.h
@@ -4,7 +4,9 @@
 
 + (instancetype)sharedInstance;
 
-- (void)observer:(BBObserver *)observer addBulletin:(BBBulletin *)bulletin forFeed:(NSUInteger)feed;
+- (void)observer:(BBObserver *)observer addBulletin:(BBBulletin *)bulletin forFeed:(NSUInteger)feed; // 7.0 - 8.1
+- (void)observer:(BBObserver *)observer addBulletin:(BBBulletin *)bulletin forFeed:(NSUInteger)feed playLightsAndSirens:(BOOL)playLightsAndSirens withReply:(id)reply; // 8.2 -
+
 - (void)showTestBanner; // 5.0 - 6.1
 
 @end
