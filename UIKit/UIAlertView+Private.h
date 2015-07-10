7826ba902136f80ced7de51bfe01a4697bfbd142
Adam Demasi <kirbylover4000@gmail.com>
2015-07-10 13:37:59 +0930

    [UIKit] add weird UIAlertView context and text field properties

--- UIKit/UIAlertView+Private.h
+++ UIKit/UIAlertView+Private.h
@@ -1,5 +1,8 @@
 @interface UIAlertView (Private)
 
+@property (nonatomic, retain) NSString *context;
+@property (nonatomic, retain, readonly) UITextField *textField;
+
 @property BOOL forceHorizontalButtonsLayout;
 @property NSInteger numberOfRows;
 
