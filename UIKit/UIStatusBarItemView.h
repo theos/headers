cb426e0bc2726c47848c2f723761b464449a1d2b
Adam Demasi <adam@hbang.com.au>
2015-08-07 12:49:19 +0930

    [UIKit] add UIStatusBarItemViewTextStyle enum

--- UIKit/UIStatusBarItemView.h
+++ UIKit/UIStatusBarItemView.h
@@ -1,3 +1,8 @@
+typedef NS_ENUM(NSUInteger, UIStatusBarItemViewTextStyle) {
+	UIStatusBarItemViewTextStyleRegular,
+	UIStatusBarItemViewTextStyleBold
+};
+
 @interface UIStatusBarItemView : UIView
 
 - (UIImage *)imageWithShadowNamed:(NSString *)imageName;
