c95e23082924407dad09e52f59b48418a5918db0
Adam Demasi <adam@hbang.com.au>
2015-08-14 12:18:41 +0930

    [Prefix] use private CFLog function so we log to stderr when needed

--- _Prefix/HBLog.h
+++ _Prefix/HBLog.h
@@ -1,4 +1,4 @@
-#include <sys/syslog.h>
+#include <CoreFoundation/CFLogUtilities.h>
 
 #define NSLog(...) { \
 	_Pragma("message(\"NSLog is deprecated. Try HBLogDebug, HBLogInfo, and HBLogError.\")"); \
@@ -6,19 +6,19 @@
 }
 
 #ifdef __DEBUG__
-	#define HB_LOG_FORMAT(color) "\e[1;3" #color "m[%s] \e[m\e[0;3" #color "m%s:%d\e[m \e[0;30;4" #color "m%s:\e[m %s"
+	#define HB_LOG_FORMAT(color) CFSTR("\e[1;3" #color "m[%s] \e[m\e[0;3" #color "m%s:%d\e[m \e[0;30;4" #color "m%s:\e[m %@")
 #else
-	#define HB_LOG_FORMAT(color) "[%s: %s:%d] %s: %s"
+	#define HB_LOG_FORMAT(color) CFSTR("[%s: %s:%d] %s: %@")
 #endif
 
-#define HB_LOG_INTERNAL(color, level, type, ...) syslog(level, HB_LOG_FORMAT(color), THEOS_INSTANCE_NAME, __BASE_FILE__, __LINE__, type, [NSString stringWithFormat:__VA_ARGS__].UTF8String);
+#define HB_LOG_INTERNAL(color, level, type, ...) CFLog(level, HB_LOG_FORMAT(color), THEOS_INSTANCE_NAME, __BASE_FILE__, __LINE__, type, (CFStringRef)[NSString stringWithFormat:__VA_ARGS__]);
 
 #ifdef __DEBUG__
-	#define HBLogDebug(...) HB_LOG_INTERNAL(6, LOG_NOTICE, "DEBUG", __VA_ARGS__)
+	#define HBLogDebug(...) HB_LOG_INTERNAL(6, kCFLogLevelNotice, "DEBUG", __VA_ARGS__)
 #else
 	#define HBLogDebug(...)
 #endif
 
-#define HBLogInfo(...) HB_LOG_INTERNAL(2, LOG_NOTICE, "INFO", __VA_ARGS__)
-#define HBLogWarn(...) HB_LOG_INTERNAL(3, LOG_WARNING, "WARN", __VA_ARGS__)
-#define HBLogError(...) HB_LOG_INTERNAL(1, LOG_ERR, "ERROR", __VA_ARGS__)
+#define HBLogInfo(...) HB_LOG_INTERNAL(2, kCFLogLevelNotice, "INFO", __VA_ARGS__)
+#define HBLogWarn(...) HB_LOG_INTERNAL(3, kCFLogLevelWarning, "WARN", __VA_ARGS__)
+#define HBLogError(...) HB_LOG_INTERNAL(1, kCFLogLevelError, "ERROR", __VA_ARGS__)
