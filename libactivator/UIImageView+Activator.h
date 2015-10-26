ddc65151476e122f019b1845e572c66dd150e31f
Adam Demasi <adam@hbang.com.au>
2015-10-26 23:25:32 +1030

    [CaptainHook, libactivator] use both as submodules also

--- libactivator/UIImageView+Activator.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface UIImageView (Activator)
-@property (nonatomic, assign) BOOL activatorListenerImageIsThreaded;
-@property (nonatomic, copy) NSString *activatorListenerName;
-@end
--- libactivator/libactivator.h
+++
@@ -1,7 +0,0 @@
-#import "LAActivator.h"
-#import "LAActivatorVersion.h"
-#import "LAEvent.h"
-#import "LAEventDataSource.h"
-#import "LAListener.h"
-#import "LASettingsViewController.h"
-#import "UIImageView+Activator.h"
