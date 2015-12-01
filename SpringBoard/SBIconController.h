9f10b50bbcc5e1fbd3d0ce880cec1cdd6aa40d0a
Adam Demasi <adam@hbang.com.au>
2015-12-02 01:52:54 +1030

    [SpringBoard] various icon, icon list, folder, etc classes

--- SpringBoard/SBIconController.h
+++ SpringBoard/SBIconController.h
@@ -1,9 +1,14 @@
-@class SBIconModel;
+@class SBDockIconListView, SBIconModel, SBRootIconListView, SBRootFolderController;
 
 @interface SBIconController : NSObject
 
 + (instancetype)sharedInstance;
 
 @property (nonatomic, retain) SBIconModel *model;
+
+@property (nonatomic, retain, readonly) SBRootIconListView *currentRootIconList;
+@property (nonatomic, retain, readonly) SBDockIconListView *dockListView;
+
+@property (nonatomic, retain, readonly) SBRootFolderController *_rootFolderController;
 
 @end
