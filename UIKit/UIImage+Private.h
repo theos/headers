00e782ca3960a0a24d9e724769b0756c5160a220
Ben Rosen <benrosen78@gmail.com>
2015-12-05 12:03:58 -0600

    [UIKit] add cpbitmap read/write methods to uiimage

--- UIKit/UIImage+Private.h
+++ UIKit/UIImage+Private.h
@@ -14,12 +14,16 @@ __END_DECLS
 + (instancetype)kitImageNamed:(NSString *)name;
 + (instancetype)imageNamed:(NSString *)name inBundle:(NSBundle *)bundle;
 
++ (instancetype)imageWithContentsOfCPBitmapFile:(NSString *)filename flags:(NSInteger)flags; // TODO: make this an enum
+
 + (instancetype)_applicationIconImageForBundleIdentifier:(NSString *)bundleIdentifier format:(MIIconVariant)format scale:(CGFloat)scale;
 + (instancetype)_iconForResourceProxy:(LSApplicationProxy *)applicationProxy format:(MIIconVariant)format;
 
 - (instancetype)_applicationIconImageForFormat:(MIIconVariant)format precomposed:(BOOL)precomposed scale:(CGFloat)scale;
 
 - (instancetype)_flatImageWithColor:(UIColor *)color;
+
+- (BOOL)writeToCPBitmapFile:(NSString *)filename flags:(NSInteger)flags; // TODO: make this an enum
 
 @property CGFloat scale;
 
