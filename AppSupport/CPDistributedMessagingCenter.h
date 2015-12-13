bf11a36ea6eb294e40fa26925c998ac63960f9e9
Ben Rosen <benrosen78@gmail.com>
2015-12-12 18:45:00 -0600

    [AppSupport] add in error handling method to CPDistributedMessagingCenter

--- AppSupport/CPDistributedMessagingCenter.h
+++ AppSupport/CPDistributedMessagingCenter.h
@@ -4,6 +4,8 @@
 
 - (void)runServerOnCurrentThread;
 - (void)registerForMessageName:(NSString *)messageName target:(id)target selector:(SEL)selector;
+
 - (id)sendMessageAndReceiveReplyName:(NSString *)messageName userInfo:(id)userInfo;
+- (id)sendMessageAndReceiveReplyName:(NSString *)messageName userInfo:(id)userInfo error:(NSError **)error;
 
 @end
