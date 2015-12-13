a7e5674589678f27effd4d20b3ea1e2c94245ef9
Ben Rosen <benrosen78@gmail.com>
2015-12-12 21:31:10 -0600

    [AppSupport] add in a method to send messages w CPDistributedMessagingCenter

--- AppSupport/CPDistributedMessagingCenter.h
+++ AppSupport/CPDistributedMessagingCenter.h
@@ -2,10 +2,13 @@
 
 + (instancetype)centerNamed:(NSString *)name;
 
+- (void)runServer;
 - (void)runServerOnCurrentThread;
 - (void)registerForMessageName:(NSString *)messageName target:(id)target selector:(SEL)selector;
 
-- (id)sendMessageAndReceiveReplyName:(NSString *)messageName userInfo:(id)userInfo;
-- (id)sendMessageAndReceiveReplyName:(NSString *)messageName userInfo:(id)userInfo error:(NSError **)error;
+- (BOOL)sendMessageName:(NSString *)messageName userInfo:(NSDictionary *)userInfo;
+
+- (NSDictionary *)sendMessageAndReceiveReplyName:(NSString *)messageName userInfo:(NSDictionary *)userInfo;
+- (NSDictionary *)sendMessageAndReceiveReplyName:(NSString *)messageName userInfo:(NSDictionary *)userInfo error:(NSError **)error;
 
 @end
