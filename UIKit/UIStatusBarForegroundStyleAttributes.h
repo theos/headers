90ccfd67869d4d014ea70de11601b9c59c269988
Adam Demasi <adam@hbang.com.au>
2016-02-06 17:55:49 +1030

    [UIKit] add UIStatusBar…StyleAttributes.height and textFontForStyle:

--- UIKit/UIStatusBarForegroundStyleAttributes.h
+++ UIKit/UIStatusBarForegroundStyleAttributes.h
@@ -1,4 +1,10 @@
+#import "UIStatusBarItemView.h"
+
 @interface UIStatusBarForegroundStyleAttributes : NSObject
+
+@property (nonatomic) CGFloat height;
+
+- (UIFont *)textFontForStyle:(UIStatusBarItemViewTextStyle)style;
 
 - (NSString *)expandedNameForImageName:(NSString *)imageName;
 - (UIImage *)untintedImageNamed:(NSString *)name;
