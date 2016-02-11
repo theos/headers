d4436a728e1c4df0508f8fa22045043fa0f3a07d
Adam Demasi <adam@hbang.com.au>
2016-02-12 09:24:48 +1030

    [IMCore] IMChat, IMHandle, IMItemsController

--- IMCore/IMHandle.h
+++ IMCore/IMHandle.h
@@ -2,8 +2,10 @@
 
 @interface IMHandle : NSObject
 
-@property (nonatomic, retain) NSString *_displayNameWithAbbreviation;
+@property (nonatomic, retain, readonly) NSString *ID;
 
 @property (nonatomic, retain) IMPerson *person;
+
+@property (nonatomic, retain) NSString *_displayNameWithAbbreviation;
 
 @end
