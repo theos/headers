4f5b319a63168ba62585198d40427c7ed57e3e60
Adam Demasi <adam@hbang.com.au>
2016-02-25 20:06:25 +1030

    [BulletinBoard] add +[BBAction actionWithCallblock:] and typedef

--- BulletinBoard/BBAction.h
+++ BulletinBoard/BBAction.h
@@ -1,8 +1,11 @@
+typedef void (^BBActionCallblock)();
+
 @interface BBAction : NSObject
 
 + (instancetype)action;
-+ (instancetype)actionWithLaunchBundleID:(NSString *)bundleID callblock:(id)block;
-+ (instancetype)actionWithLaunchURL:(NSURL *)url callblock:(id)block;
++ (instancetype)actionWithLaunchBundleID:(NSString *)bundleID callblock:(BBActionCallblock)callblock;
++ (instancetype)actionWithLaunchURL:(NSURL *)url callblock:(BBActionCallblock)callblock;
++ (instancetype)actionWithCallblock:(BBActionCallblock)callblock;
 
 @property (nonatomic, retain) NSString *bundleID;
 
