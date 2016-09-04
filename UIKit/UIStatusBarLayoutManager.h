21460e033b1da7882d9629ca5b638d0fdba72101
Adam Demasi <adam@hbang.com.au>
2016-09-04 19:06:13 +0930

    [UIKit] this should still work without throwing syntax errors

--- UIKit/UIStatusBarLayoutManager.h
+++ UIKit/UIStatusBarLayoutManager.h
@@ -3,7 +3,7 @@
 @interface UIStatusBarLayoutManager : NSObject
 
 @property (nonatomic, retain) UIStatusBarForegroundView *foregroundView;
-@property (nonatomic, retain, readonly) UIStatusBarItemView **_itemViews;
+@property (nonatomic, retain, readonly) UIStatusBarItemView *_itemViews;
 
 @property BOOL persistentAnimationsEnabled;
 
