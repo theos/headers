9b20742c71f40b1aa96612f0499f96a9e55aa472
Thatchapon Unprasert <poomst@hotmail.com>
2017-07-07 11:54:24 +0700

    Updated.

--- UIKit/UIKBRenderFactory.h
+++ UIKit/UIKBRenderFactory.h
@@ -1,7 +1,9 @@
-#import <Foundation/Foundation.h>
+#import "UIKBRenderConfig.h"
 
 @interface UIKBRenderFactory : NSObject
 + (BOOL)_enabled;
 + (NSInteger)_graphicsQuality;
 - (UIKBRenderConfig *)renderConfig;
+- (NSString *)deleteKeyImageName;
+- (NSString *)deleteOnKeyImageName; // iOS 9.0+
 @end
