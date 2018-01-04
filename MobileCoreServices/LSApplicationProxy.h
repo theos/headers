cf26cd5659d50869f9ab2206c993112116c30e3b
Adam Demasi <adam@hbang.com.au>
2018-01-04 13:09:27 +1030

    [MobileCoreServices] Some new methods + fixing class hierarchy

--- MobileCoreServices/LSApplicationProxy.h
+++ MobileCoreServices/LSApplicationProxy.h
@@ -1,9 +1,10 @@
-@interface LSApplicationProxy : NSObject
+#import "LSBundleProxy.h"
+
+@interface LSApplicationProxy : LSBundleProxy
 
 + (instancetype)applicationProxyForIdentifier:(NSString *)identifier;
 
 @property (nonatomic, retain) NSString *applicationIdentifier;
-@property (nonatomic, retain) NSString *localizedName;
 
 @property (readonly) BOOL isInstalled;
 
