9246d17556bfb4e9e5ba05f90ca3983dd7ad28d3
Adam Demasi <adam@hbang.com.au>
2018-01-09 17:07:24 +1030

    [Preferences] Clean up what looks like a direct class-dump header.

--- Preferences/PSSwitchTableCell.h
+++ Preferences/PSSwitchTableCell.h
@@ -2,23 +2,10 @@
 
 @class UIActivityIndicatorView;
 
-@interface PSSwitchTableCell : PSControlTableCell  {
-    UIActivityIndicatorView *_activityIndicator;
+@interface PSSwitchTableCell : PSControlTableCell {
+	UIActivityIndicatorView *_activityIndicator;
 }
 
-@property BOOL loading;
-
-- (BOOL)loading;
-- (void)setLoading:(BOOL)loading;
-- (id)controlValue;
-- (id)newControl;
-- (void)setCellEnabled:(BOOL)enabled;
-- (void)refreshCellContentsWithSpecifier:(id)specifier;
-- (id)initWithStyle:(int)style reuseIdentifier:(id)identifier specifier:(id)specifier;
-- (void)reloadWithSpecifier:(id)specifier animated:(BOOL)animated;
-- (BOOL)canReload;
-- (void)prepareForReuse;
-- (void)layoutSubviews;
-- (void)setValue:(id)value;
+@property (nonatomic) BOOL loading;
 
 @end
