cddc59b2c2d2ed2bb01ca4839da607cde677b3bc
Shade-Zepheri <ziroalpha@gmail.com>
2018-04-20 20:51:00 -0500

    [SpringBoard] Fixed commas because we're nitpicky

--- SpringBoard/SBAppViewController.h
+++ SpringBoard/SBAppViewController.h
@@ -4,13 +4,13 @@
 @class SBApplication, SBAppView, SBWorkspaceApplication. SBApplicationSceneEntity;
 
 @interface SBAppViewController : UIViewController <SBApplicationHosting>
-@property (nonatomic,copy,readonly) NSString *bundleIdentifier;
-@property (assign,nonatomic) BOOL automatesLifecycle;
-@property (assign,nonatomic) NSInteger requestedMode;
-@property (nonatomic,readonly) NSInteger currentMode;
-@property (nonatomic,readonly) SBAppView *appView;
-@property (assign,nonatomic) NSUInteger options;
-@property (assign,nonatomic) BOOL ignoresOcclusions;
+@property (nonatomic, copy, readonly) NSString *bundleIdentifier;
+@property (assign, nonatomic) BOOL automatesLifecycle;
+@property (assign, nonatomic) NSInteger requestedMode;
+@property (nonatomic, readonly) NSInteger currentMode;
+@property (nonatomic, readonly) SBAppView *appView;
+@property (assign, nonatomic) NSUInteger options;
+@property (assign, nonatomic) BOOL ignoresOcclusions;
 
 - (instancetype)initWithIdentifier:(NSString *)identifier andApplication:(SBWorkspaceApplication *)application NS_DEPRECATED_IOS(8_0, 11_0);
 - (instancetype)initWithIdentifier:(NSString *)identifier andApplicationSceneEntity:(SBApplicationSceneEntity *)applicationSceneEntity NS_AVAILABLE_IOS(11_0);
