4d27cfdd7a15307de6df54300d23fcf24c5f1e35
Leptos <leptos.0.null@gmail.com>
2019-05-25 17:54:04 -0700

    [BatteryCenter] Add BatteryDevice class and manager

---
+++ BatteryCenter/BCBatteryDeviceController.h
@@ -0,0 +1,13 @@
+#import "BCBatteryDevice.h"
+
+API_AVAILABLE(ios(9.0))
+@interface BCBatteryDeviceController : NSObject
+
+@property (nonatomic, readonly) NSArray<BCBatteryDevice *> *connectedDevices;
+@property (nonatomic, readonly) NSString *connectedDevicesDidChangeNotificationName;
+
++ (instancetype)sharedInstance;
+
+- (void)removeDeviceChangeHandlerWithIdentifier:(NSString *)identifier;
+
+@end
