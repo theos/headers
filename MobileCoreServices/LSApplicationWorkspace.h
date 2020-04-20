430f091e3a8a9c687692ab183b46df30e0fbb69c
Leptos <leptos.0.null@gmail.com>
2020-04-20 13:04:14 -0700

    [MobileCoreServices] Add availability attributes to Workspace
    Availability determined primarily from developer.limneos.net

--- MobileCoreServices/LSApplicationWorkspace.h
+++ MobileCoreServices/LSApplicationWorkspace.h
@@ -1,32 +1,54 @@
 #import "LSApplicationProxy.h"
+#import "LSDocumentProxy.h"
+#import "LSPlugInKitProxy.h"
 
+API_AVAILABLE(ios(5.0))
 @interface LSApplicationWorkspace : NSObject
 
 + (instancetype)defaultWorkspace;
 
-- (void)openApplicationWithBundleID:(NSString *)bundleID;
+- (NSArray<LSApplicationProxy *> *)allApplications          API_AVAILABLE(ios(7.0));
+- (NSArray<LSApplicationProxy *> *)allInstalledApplications API_AVAILABLE(ios(7.0));
+- (NSArray<LSApplicationProxy *> *)directionsApplications   API_AVAILABLE(ios(6.0));
+- (NSArray<LSApplicationProxy *> *)unrestrictedApplications API_AVAILABLE(ios(7.0));
 
-- (NSArray<LSApplicationProxy *> *)allApplications;
-- (NSArray<LSApplicationProxy *> *)allInstalledApplications;
-- (NSArray<LSApplicationProxy *> *)directionsApplications;
-- (NSArray<LSApplicationProxy *> *)unrestrictedApplications;
+- (NSArray<NSString *> *)removedSystemApplications    API_AVAILABLE(ios(9.3));
+- (BOOL)restoreSystemApplication:(NSString *)bundleID API_AVAILABLE(ios(9.3));
 
-- (NSArray<NSString *> *)removedSystemApplications;
+- (NSArray<LSApplicationProxy *> *)applicationsAvailableForHandlingURLScheme:(NSString *)urlScheme;
+- (NSArray<LSApplicationProxy *> *)applicationsAvailableForOpeningDocument:(LSDocumentProxy *)document;
+- (NSArray<LSApplicationProxy *> *)applicationsAvailableForOpeningURL:(NSURL *)url API_AVAILABLE(ios(10.0));
+- (NSArray<LSApplicationProxy *> *)applicationsAvailableForOpeningURL:(NSURL *)url legacySPI:(BOOL)legacySPI API_AVAILABLE(ios(10.0));
 
-- (NSArray *)applicationsAvailableForHandlingURLScheme:(NSString *)urlScheme;
-- (NSArray *)applicationsAvailableForOpeningURL:(NSURL *)url;
-- (NSArray *)applicationsAvailableForOpeningURL:(NSURL *)url legacySPI:(BOOL)legacySPI;
+- (LSApplicationProxy *)applicationForUserActivityDomainName:(NSString *)domain API_AVAILABLE(ios(8.0));
+- (LSApplicationProxy *)applicationForUserActivityType:(NSString *)activityType API_AVAILABLE(ios(8.0));
+- (NSArray<LSApplicationProxy *> *)applicationsForUserActivityType:(NSString *)activityType API_AVAILABLE(ios(10.0));
+- (NSArray<LSApplicationProxy *> *)applicationsForUserActivityType:(NSString *)activityType limit:(NSUInteger)limit API_AVAILABLE(ios(10.0));;
+- (BOOL)getClaimedActivityTypes:(NSSet<NSString *> **)activityTypes domains:(NSSet<NSString *> **)domains API_AVAILABLE(ios(8.0));
 
-- (NSURL *)URLOverrideForURL:(NSURL *)url;
+- (NSURL *)URLOverrideForURL:(NSURL *)url API_AVAILABLE(ios(6.0));
+
+- (NSArray<NSString *> *)publicURLSchemes  API_AVAILABLE(ios(6.0));
+- (NSArray<NSString *> *)privateURLSchemes API_AVAILABLE(ios(6.0));
+
+// convience method for the respective openURL with `LSOpenSensitiveURLOption : YES` set in options
+- (BOOL)openSensitiveURL:(NSURL *)url withOptions:(NSDictionary<NSString *, id> *)options API_AVAILABLE(ios(6.0));
+- (BOOL)openSensitiveURL:(NSURL *)url withOptions:(NSDictionary<NSString *, id> *)options error:(NSError **)error API_AVAILABLE(ios(9.0));
+
+- (BOOL)openURL:(NSURL *)url API_AVAILABLE(ios(6.0));
+- (BOOL)openURL:(NSURL *)url withOptions:(NSDictionary<NSString *, id> *)options API_AVAILABLE(ios(6.0));
+- (BOOL)openURL:(NSURL *)url withOptions:(NSDictionary<NSString *, id> *)options error:(NSError **)error API_AVAILABLE(ios(6.0));
+
+- (void)openApplicationWithBundleID:(NSString *)bundleID API_AVAILABLE(ios(7.0));
 
 - (BOOL)registerApplication:(NSURL *)url;
-- (BOOL)registerPlugin:(NSURL *)url;
-
 - (BOOL)unregisterApplication:(NSURL *)url;
-- (BOOL)unregisterPlugin:(NSURL *)url;
 
-- (BOOL)openURL:(NSURL *)url;
+- (BOOL)registerPlugin:(NSURL *)url   API_AVAILABLE(ios(8.0));
+- (BOOL)unregisterPlugin:(NSURL *)url API_AVAILABLE(ios(8.0));
 
-- (NSArray *)pluginsWithIdentifiers:(NSArray *)identifiers protocols:(NSArray *)protocols version:(id)version;
+// version unused as of iOS 10.2
+// returns the union of matches. pass empty arrays to get all plugins
+- (NSArray<LSPlugInKitProxy *> *)pluginsWithIdentifiers:(NSArray<NSString *> *)identifiers protocols:(NSArray<NSString *> *)protocols version:(id)version API_AVAILABLE(ios(8.0));
 
 @end
--- MobileCoreServices/LSPlugInKitProxy.h
+++ MobileCoreServices/LSPlugInKitProxy.h
@@ -1,5 +1,6 @@
 #import "LSBundleProxy.h"
 
+API_AVAILABLE(ios(8.0))
 @interface LSPlugInKitProxy : LSBundleProxy
 
 + (instancetype)pluginKitProxyForIdentifier:(NSString *)identifier;
@@ -7,12 +8,12 @@
 
 @property (nonatomic, readonly) LSBundleProxy *containingBundle;
 @property (nonatomic, readonly) NSDictionary *infoPlist;
-@property (nonatomic, readonly) NSString *originalIdentifier;
 @property (nonatomic, readonly) NSString *pluginIdentifier;
+@property (nonatomic, readonly) NSString *originalIdentifier API_AVAILABLE(ios(9.0));
 
 @property (nonatomic, readonly) NSUUID *pluginUUID;
 @property (nonatomic, readonly) NSString *protocol;
 @property (nonatomic, readonly) NSDate *registrationDate;
-@property (nonatomic, readonly) NSString *teamID;
+@property (nonatomic, readonly) NSString *teamID API_AVAILABLE(ios(10.0));
 
 @end
