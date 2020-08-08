26799a530b18f1ea2d30e1817c0eb1c25e8fc8c3
Adam Demasi <adam@hbang.com.au>
2020-08-08 19:08:53 +0930

    [BaseBoard] Add BSPlatform

---
+++ BaseBoard/BSPlatform.h
@@ -0,0 +1,39 @@
+@import Foundation;
+
+typedef NS_ENUM(NSUInteger, BSDeviceClass) {
+	BSDeviceClassIPhone,
+	BSDeviceClassIPod,
+	BSDeviceClassIPad
+};
+
+typedef NS_ENUM(NSUInteger, BSHomeButtonType) {
+	BSHomeButtonTypeButton = 0,
+	BSHomeButtonTypeBorderless = 2
+};
+
+@interface BSPlatform : NSObject
+
++ (instancetype)sharedInstance;
+
+@property (nonatomic, readonly) BSDeviceClass deviceClass;
+@property (nonatomic, readonly) BOOL hasGasGauge;
+@property (nonatomic, readonly) BOOL hasOrbCapability;
+@property (nonatomic, readonly) BSHomeButtonType homeButtonType;
+@property (getter=isMultiCore, nonatomic, readonly) BOOL multiCore;
+
+@property (nonatomic, readonly, copy) NSString *localizedProductName;
+@property (nonatomic, readonly, copy) NSString *productClass;
+@property (nonatomic, readonly, copy) NSString *productHardwareModel;
+@property (nonatomic, readonly, copy) NSString *productHardwareModelName;
+@property (nonatomic, readonly, copy) NSString *productType;
+@property (nonatomic, readonly, copy) NSString *uniqueDeviceIdentifier;
+
+@property (nonatomic, readonly, copy) NSString *operatingSystemName;
+@property (nonatomic, readonly, copy) NSString *productBuildVersion;
+@property (nonatomic, readonly, copy) NSString *productVersion;
+
+@property (getter=isCarrierInstall, nonatomic, readonly) BOOL carrierInstall;
+@property (getter=isDeveloperInstall, nonatomic, readonly) BOOL developerInstall;
+@property (getter=isInternalInstall, nonatomic, readonly) BOOL internalInstall;
+
+@end
