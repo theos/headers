bad274f027be20707d3a4eadd36023f9bb327afc
Ryan Nair <ryannair05@gmail.com>
2021-02-13 18:27:54 -0500

    Update SBIconListView.h

--- SpringBoard/SBIconListView.h
+++ SpringBoard/SBIconListView.h
@@ -1,4 +1,9 @@
-@interface SBIconListView : UIView
+NS_CLASS_AVAILABLE_IOS(4_0) @interface SBIconListView : UIView
+
+@property(readonly, nonatomic) NSUInteger maximumIconCount API_AVAILABLE(ios(13.0));
+@property(readonly, nonatomic) NSUInteger maxIcons NS_DEPRECATED_IOS(4_0, 13_0);
+@property(readonly, nonatomic) NSUInteger iconColumnsForCurrentOrientation;
+@property(readonly, nonatomic) NSUInteger iconRowsForCurrentOrientation;
 
 - (void)setAlphaForAllIcons:(CGFloat)alpha;
 
