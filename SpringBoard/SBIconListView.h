9c5e3fd848733c081b12393f7f0e30510ec79732
Ryan Nair <ryannair05@gmail.com>
2021-02-15 20:35:10 -0500

    Update SBIconListView.h (#37)
    * Update SBIconListView.h
    
    * Update SBIconListView.h
    
    Change NS_DEPRECATED_IOS to API_DEPRECATED_WITH_REPLACEMENT

--- SpringBoard/SBIconListView.h
+++ SpringBoard/SBIconListView.h
@@ -1,4 +1,9 @@
-@interface SBIconListView : UIView
+NS_CLASS_AVAILABLE_IOS(4_0) @interface SBIconListView : UIView
+
+@property(readonly, nonatomic) NSUInteger maximumIconCount API_AVAILABLE(ios(13.0));
+@property(readonly, nonatomic) NSUInteger maxIcons API_DEPRECATED_WITH_REPLACEMENT("maximumIconCount", ios(4.0, 13.0));
+@property(readonly, nonatomic) NSUInteger iconColumnsForCurrentOrientation;
+@property(readonly, nonatomic) NSUInteger iconRowsForCurrentOrientation;
 
 - (void)setAlphaForAllIcons:(CGFloat)alpha;
 
