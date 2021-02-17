00dd75f94b46ae12d1249e393dfd7836d4c1cb03
Tanner Bennett <tannerbennett@me.com>
2021-02-16 20:27:55 -0600

    Resolve std*11 warning in LightMessaging.h (#35)
    `serverName` should be `const name_t` not just `name_t` or you get a warning for pointer decay from a string literal into `char *`

--- rocketbootstrap/rocketbootstrap.h
+++ rocketbootstrap/rocketbootstrap.h
@@ -9,7 +9,7 @@ kern_return_t rocketbootstrap_look_up(mach_port_t bp, const name_t service_name,
 
 
 kern_return_t rocketbootstrap_unlock(const name_t service_name); // Errors if not in a privileged process such as SpringBoard or backboardd
-kern_return_t rocketbootstrap_register(mach_port_t bp, name_t service_name, mach_port_t sp); // Errors if not in a privileged process such as SpringBoard or backboardd
+kern_return_t rocketbootstrap_register(mach_port_t bp, const name_t service_name, mach_port_t sp); // Errors if not in a privileged process such as SpringBoard or backboardd
 
 #ifdef __COREFOUNDATION_CFMESSAGEPORT__
 CFMessagePortRef rocketbootstrap_cfmessageportcreateremote(CFAllocatorRef allocator, CFStringRef name);
