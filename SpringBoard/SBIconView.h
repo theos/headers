69202fd9c1fc33f07cc86e50b585d73254931a48
PoomSmart <thatchapon.unp@outlook.com>
2021-03-06 15:56:39 +0700

    SBIconView location

--- SpringBoard/SBIconView.h
+++ SpringBoard/SBIconView.h
@@ -1,10 +1,13 @@
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 @class SBIcon;
 
 @interface SBIconView : UIView
 
 + (CGSize)defaultIconSize;
+
+- (NSString *)location;
 
 @property (nonatomic, retain) SBIcon *icon;
 
