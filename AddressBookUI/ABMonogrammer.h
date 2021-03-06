5954622f419d95675047e35767aa1faa1f2898f0
Leptos <leptos.0.null@gmail.com>
2021-03-06 09:02:07 -0800

    [ABMonogrammer] ABMonogrammer was removed in iOS 11

--- AddressBookUI/ABMonogrammer.h
+++ AddressBookUI/ABMonogrammer.h
@@ -1,3 +1,5 @@
+#import <Foundation/Foundation.h>
+
 typedef NS_ENUM(NSUInteger, ABMonogrammerStyle) {
 	ABMonogrammerStyleLightGray,
 	ABMonogrammerStyleWhite,
@@ -5,6 +7,11 @@ typedef NS_ENUM(NSUInteger, ABMonogrammerStyle) {
 	ABMonogrammerStyleLightGraySemitransparent,
 };
 
+/* Removed in iOS 11
+ * superclass changed to CNMonogrammer in iOS 9,
+ *   and almost all functionality was moved there as well
+ */
+API_DEPRECATED_WITH_REPLACEMENT("CNMonogrammer", ios(7.0, 11.0))
 @interface ABMonogrammer : NSObject
 
 - (instancetype)initWithStyle:(ABMonogrammerStyle)style diameter:(CGFloat)diameter;
