2d9bcc848815627844ebce37f78d72f8f30c4aa3
Adam Demasi <adam@hbang.com.au>
2021-03-10 17:04:18 +1030

    [Foundation] Remove broken and no longer needed XPC headers.

--- Foundation/NSXPCInterface.h
+++
@@ -1,9 +0,0 @@
-@interface NSXPCInterface : NSObject
-
-+ (id)interfaceWithProtocol:(id)arg1;
-
-@property (retain, nonatomic) Protocol *protocol;
-
-- (void)setClasses:(NSSet *)classes forSelector:(SEL)selector argumentIndex:(NSUInteger)index ofReply:(BOOL)reply;
-
-@end
