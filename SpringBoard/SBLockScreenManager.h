50235cfaa42697704eccc2b893c9b7347326c97e
Ryan Nair <ryannair05@gmail.com>
2021-03-14 10:32:34 -0400

    Commit requested changes
    Co-authored-by: Thatchapon Unprasert <thatchapon.unp@outlook.com>

--- SpringBoard/SBLockScreenManager.h
+++ SpringBoard/SBLockScreenManager.h
@@ -8,7 +8,7 @@ API_AVAILABLE(ios(7.0)) @interface SBLockScreenManager : NSObject
 - (void)unlockUIFromSource:(NSUInteger)source withOptions:(NSDictionary *)options;
 
 @property (nonatomic, retain) SBLockScreenViewController *lockScreenViewController API_DEPRECATED_WITH_REPLACEMENT("coverSheetViewController", ios(7.0, 13.0));
-@property (nonatomic,readonly) CSCoverSheetViewController *coverSheetViewController API_AVAILABLE(ios(13.0));
+@property (nonatomic, readonly) CSCoverSheetViewController *coverSheetViewController API_AVAILABLE(ios(13.0));
 
 @property (readonly) BOOL isUILocked;
 
