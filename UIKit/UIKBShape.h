2953181e6c933e00c7480bd1d1c488acfd988d75
PoomSmart <thatchapon.unp@outlook.com>
2021-04-06 16:02:23 +0700

    [UIKit] Added some method to UIKBShape

--- UIKit/UIKBShape.h
+++ UIKit/UIKBShape.h
@@ -7,6 +7,8 @@
 @property CGRect symbolFrame;
 @property CGRect frame;
 
+- (instancetype)initWithGeometry:(UIKBGeometry *)geometry frame:(CGRect)frame paddedFrame:(CGRect)paddedFrame;
+
 - (CGRect)originalFrame;
 
 @end
