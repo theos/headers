3df2a1f223764a4e295bfb4b873edabd2ac5768c
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2021-05-30 22:36:16 +0700

    Update CCUIToggleModule.h

--- ControlCenterUIKit/CCUIToggleModule.h
+++ ControlCenterUIKit/CCUIToggleModule.h
@@ -6,13 +6,13 @@ API_AVAILABLE(ios(11.0))
 
 @property (assign, getter=isSelected, nonatomic) BOOL selected;
 
-@property (nonatomic, copy, readonly) UIImage *iconGlyph;
-@property (nonatomic, copy, readonly) UIImage *selectedIconGlyph;
-
 /*
  * If you're using an image as you icon gylph, Icon glyphs should have
  * a height of 48px for @2x and 72 for @3x, the width can be anything.
  */
+@property (nonatomic, copy, readonly) UIImage *iconGlyph;
+@property (nonatomic, copy, readonly) UIImage *selectedIconGlyph;
+
 @property (nonatomic, copy, readonly) UIColor *selectedColor;
 
 @property (nonatomic, copy, readonly) CCUICAPackageDescription *glyphPackageDescription;
