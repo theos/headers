e9c8f7d3f5c850ac8237497e150208915a9eb1d2
PoomSmart <thatchapon.unp@outlook.com>
2021-05-30 14:12:37 +0700

    [ControlCenterUIKit, QuartzCore, UIKit] New classes for ControlCenterUIKit
    Used by Control Center module tweaks

---
+++ ControlCenterUIKit/CCUICAPackageDescription.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+
+API_AVAILABLE(ios(11.0))
+@interface CCUICAPackageDescription : NSObject
+
+@property (nonatomic, copy, readonly) NSURL *packageURL;
+
+@property (assign, nonatomic) BOOL flipsForRightToLeftLayoutDirection;
+
++ (instancetype)descriptionForPackageNamed:(NSString *)name inBundle:(NSBundle *)bundle;
+
+- (BOOL)flipsForRightToLeftLayoutDirection;
+
+- (CCUICAPackageDescription *)initWithPackageName:(NSString *)name inBundle:(NSBundle *)bundle;
+
+- (void)setFlipsForRightToLeftLayoutDirection:(BOOL)flips;
+
+- (NSURL *)packageURL;
+
+@end
