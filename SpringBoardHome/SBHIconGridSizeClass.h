3908b38581145d7997ee4015f4eecbd56682dbce
PoomSmart <thatchapon.unp@outlook.com>
2021-06-12 16:05:11 +0700

    [SpringBoardHome] Fixed enum naming

--- SpringBoardHome/SBHIconGridSizeClass.h
+++ SpringBoardHome/SBHIconGridSizeClass.h
@@ -1,9 +1,9 @@
 #import <Foundation/Foundation.h>
 
 typedef NS_ENUM(NSInteger, SBHIconGridSizeClass) {
-    SBHIconGridSizeDefault = 0,
-    SBHIconGridSizeSmall,
-    SBHIconGridSizeMedium,
-    SBHIconGridSizeLarge,
-    SBHIconGridSizeExtraLarge
+    SBHIconGridSizeClassDefault = 0,
+    SBHIconGridSizeClassSmall,
+    SBHIconGridSizeClassMedium,
+    SBHIconGridSizeClassLarge,
+    SBHIconGridSizeClassExtraLarge
 };
