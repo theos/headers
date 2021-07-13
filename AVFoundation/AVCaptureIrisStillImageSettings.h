fddb3bab4e6406667c33679a6f0aba2a8a71dc0d
PoomSmart <thatchapon.unp@outlook.com>
2021-07-13 20:31:13 +0700

    [AVFoundation] Corrected definition

--- AVFoundation/AVCaptureIrisStillImageSettings.h
+++ AVFoundation/AVCaptureIrisStillImageSettings.h
@@ -1,7 +1,7 @@
-#import "AVCaptureStillImageSettings.h"
+#import <Foundation/Foundation.h>
 
 API_DEPRECATED("Removed in iOS 10", ios(9.0, 10.0))
-@interface AVCaptureIrisStillImageSettings : AVCaptureStillImageSettings
+@interface AVCaptureIrisStillImageSettings : NSObject
 
 @property (assign, nonatomic) NSInteger irisMovieMode;
 
--- AVFoundation/AVCaptureStillImageSettings.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-
-@interface AVCaptureStillImageSettings : NSObject
-@end
-
-#pragma clang diagnostic pop
