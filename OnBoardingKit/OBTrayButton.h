bd2f8fd23268ce37b9dd204b3fd24d4f9efba8fa
PoomSmart <thatchapon.unp@outlook.com>
2021-07-17 21:57:30 +0700

    [OnBoardingKit] Added OnBoardingKit common headers (iOS 13+)

---
+++ OnBoardingKit/OBTrayButton.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+API_AVAILABLE(ios(13.0))
+@interface OBTrayButton : UIButton
+
++ (instancetype)buttonWithType:(NSInteger)type;
+
++ (CGFloat)standardHeight;
+
+@end
