ac8c2927d4adcb883925f83cfe3103d7e68a3eca
daniel <dlevi309@gmail.com>
2021-09-24 13:23:05 -0700

    whoops

--- installd/MIDaemonConfiguration.h
+++ installd/MIDaemonConfiguration.h
@@ -1,5 +1,6 @@
 #import "MIGlobalConfiguration.h"
 
+API_AVAILABLE(ios(13.0))
 @interface MIDaemonConfiguration : MIGlobalConfiguration
 
 @property (nonatomic,readonly) BOOL skipDeviceFamilyCheck; 
