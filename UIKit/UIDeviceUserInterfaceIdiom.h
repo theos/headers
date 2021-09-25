ffd1836e6861b5636df23230fc3d300e77658ed2
daniel <dlevi309@gmail.com>
2021-09-25 11:51:00 -0700

    Add UIDeviceUserInterfaceIdiom type definitions

---
+++ UIKit/UIDeviceUserInterfaceIdiom.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, UIUserInterfaceIdiom) {
+    UIUserInterfaceIdiomUnspecified = -1,
+    UIUserInterfaceIdiomPhone = 0,
+    UIUserInterfaceIdiomPad = 1,
+    UIUserInterfaceIdiomTV = 2,
+    UIUserInterfaceIdiomCarPlay = 3,
+    UIUserInterfaceIdiomWatch = 4
+};
