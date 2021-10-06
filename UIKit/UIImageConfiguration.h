30aabe179bb275646086a6249953a9f617395dcb
PoomSmart <thatchapon.unp@outlook.com>
2021-10-06 20:14:02 +0700

    [UIKit] Simplified _UIAssetManager.h

--- UIKit/UIImageConfiguration.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface UIImageConfiguration : NSObject
-
-@property (nonatomic, readonly) UITraitCollection *traitCollection;
-
-- (instancetype)_initWithTraitCollection:(UITraitCollection *)traitCollection;
-
-@end
--- UIKit/_UIAssetManager.h
+++ UIKit/_UIAssetManager.h
@@ -1,4 +1,4 @@
-#import "UIImageConfiguration.h"
+#import <UIKit/UIKit.h>
 
 API_AVAILABLE(ios(7.0))
 @interface _UIAssetManager : NSObject
