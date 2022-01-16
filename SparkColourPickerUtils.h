6fed97c6f33225222a9255a9404c5a0fa5d25fda
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2022-01-15 23:31:08 -0500

    Add libsparkcolourpicker and libsparkapplist

---
+++ SparkColourPickerUtils.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+
+@interface SparkColourPickerUtils : NSObject
++(NSString*) hexStringFromColour:(UIColor*) colour;
++(NSString*) rgbStringFromColour:(UIColor*) colour;
++(UIColor*) inverseColour:(UIColor*) colour;
++(UIColor*) colour:(UIColor*) colour withBrightness:(float) newBrightness;
++(BOOL) colourIsBlack:(UIColor*) colour;
++(BOOL) colourIsWhite:(UIColor*) colour;
++(UIColor*) colourWithString: (NSString*) colourString;
++(UIColor *)colourWithString:(NSString *)stringToConvert withFallback:(NSString*) fallback;
++(UIColor *)colourWithString:(NSString *)stringToConvert withFallbackColour:(UIColor*) fallback;
++(UIColor*) colourWithRGBString:(NSString*) stringToConvert;
++(UIColor *) colourWithHexString:(NSString *)stringToConvert;
++(BOOL) colourIsLight :(UIColor*) colour;
++(UIColor*)interpolateFrom:(UIColor*)startColour toColour:(UIColor*)endColour withPercentage:(float)percentage;
+@end
