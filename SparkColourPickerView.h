1d9accc24e8d1809e6e599c1f5c7506e2bd75ba1
Lightmann <lightmanns11@gmail.com>
2022-06-26 09:48:28 -0400

    Make others match

--- SparkColourPickerView.h
+++ SparkColourPickerView.h
@@ -2,13 +2,13 @@
 
 NS_ASSUME_NONNULL_BEGIN
 @protocol SparkColourPickerViewDelegate <NSObject>
--(void) colourPicker:(id)picker didUpdateColour:(UIColor*) colour;
+- (void)colourPicker:(id)picker didUpdateColour:(UIColor*)colour;
 @end
 
 @interface SparkColourPickerView : UIView
 // forceUIMode is used to force to dark or light mode (-1: Default, 1: Dark, 2: Light)
--(instancetype) initWithFrame:(CGRect) frame forceUIMode:(int) uiMode;
--(instancetype) initWithFrame:(CGRect) frame forceUIMode:(int) uiMode andViewSizeFactor:(float)viewSizeFactor;
+- (instancetype)initWithFrame:(CGRect)frame forceUIMode:(int)uiMode;
+- (instancetype)initWithFrame:(CGRect)frame forceUIMode:(int)uiMode andViewSizeFactor:(float)viewSizeFactor;
 
 // These two properties can be set before showing the view to set the initial colour. The unmodified colour is without alpha.
 @property (nonatomic, retain) UIColor* currentColour;
