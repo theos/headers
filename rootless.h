665fb5c63118b61ddc4d1831cbfae6686ed26901
opa334 <opa334@users.noreply.github.com>
2023-04-21 03:12:48 +0200

    Slightly improve Xina compat

--- rootless.h
+++ rootless.h
@@ -1,7 +1,7 @@
 #ifdef XINA_SUPPORT // Only define this for rootful compilations that need support for xina
 #define ROOT_PATH(cPath) [[NSFileManager defaultManager] fileExistsAtPath:@"/var/LIY"] ? "/var/jb" cPath : cPath
-#define ROOT_PATH_NS(path) ([[NSFileManager defaultManager] fileExistsAtPath:@"/var/LIY"] ? [@"/var/jb" stringByAppendingPathComponent:path] : path )
-#define ROOT_PATH_NS_VAR ROOT_PATH_NS
+#define ROOT_PATH_NS(path) [[NSFileManager defaultManager] fileExistsAtPath:@"/var/LIY"] ? @"/var/jb" path : path
+#define ROOT_PATH_NS_VAR [[NSFileManager defaultManager] fileExistsAtPath:@"/var/LIY"] ? [@"/var/jb" stringByAppendingPathComponent:path] : path
 #define ROOT_PATH_VAR(cPath)(ROOT_PATH_NS([NSString stringWithUTF8String:cPath]).fileSystemRepresentation)
 #else
 #define ROOT_PATH(cPath) THEOS_PACKAGE_INSTALL_PREFIX cPath
