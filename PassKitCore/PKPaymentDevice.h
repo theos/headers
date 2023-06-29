6a2633945f57888bb46656cc0c05a72a528c84ce
Natheer <natheer@extbh.dev>
2023-06-29 21:14:01 +0000

    Update PassKitCore/PKPaymentDevice.h
    format
    
    Co-authored-by: Leptos <leptos.0.null@gmail.com>

--- PassKitCore/PKPaymentDevice.h
+++ PassKitCore/PKPaymentDevice.h
@@ -4,5 +4,5 @@ API_AVAILABLE(ios(8.0))
 @interface PKPaymentDevice : NSObject
 // Client details contains (OS version, device type ...).
 // Used for authenticating the device to allow or deny Apple Pay use.
-+ (NSString*)clientInfoHTTPHeader;
++ (NSString *)clientInfoHTTPHeader;
 @end
