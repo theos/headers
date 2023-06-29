9367e1a236d4e44d602b25c4968063fa4a048a72
ExTBH <natheer@extbh.dev>
2023-06-29 19:44:14 +0300

    format :skull:

--- PassKitUI/PKPGSVSectionHeaderView.h
+++ PassKitUI/PKPGSVSectionHeaderView.h
@@ -3,6 +3,6 @@
 
 API_AVAILABLE(ios(11.0))
 @interface PKPGSVSectionHeaderView : UIView
--(void)addTapped;
--(void)inboxTapped API_AVAILABLE(ios(15.0));
+- (void)addTapped;
+- (void)inboxTapped API_AVAILABLE(ios(15.0));
 @end
