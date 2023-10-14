4ec2523cae7a340aaea6b13689c127b792f3fad3
Leptos <leptos.0.null@gmail.com>
2023-10-14 16:01:39 -0700

    Add back ROOT_PATH_NS_VAR

--- rootless.h
+++ rootless.h
@@ -7,10 +7,11 @@
 //     `cPath` is a C string literal such as "/Library"
 //   ROOT_PATH_NS(path)
 //     `path` is an NSString literal such as @"/Library"
+//   ROOT_PATH_NS_VAR(path)
+//     `path` is an NSString variable
 //
 // If you need to append a variable path to the install prefix (i.e. not a string literal)
-// use the `THEOS_PACKAGE_INSTALL_PREFIX` directly. In Objective-C,
-// `[@THEOS_PACKAGE_INSTALL_PREFIX stringByAppendingPathComponent:path]` may be used.
+// use the `THEOS_PACKAGE_INSTALL_PREFIX` directly.
 // In C, `strlcpy` and `strlcat` may be used to first copy `THEOS_PACKAGE_INSTALL_PREFIX`
 // into a buffer, followed by the path variable.
 
@@ -19,7 +20,9 @@
 
 #define ROOT_PATH(cPath) !access("/var/LIY", F_OK) ? "/var/jb" cPath : cPath
 #define ROOT_PATH_NS(path) !access("/var/LIY", F_OK) ? @"/var/jb" path : path
+#define ROOT_PATH_NS_VAR(path) !access("/var/LIY", F_OK) ? [@"/var/jb" stringByAppendingPathComponent:path] : path
 #else
 #define ROOT_PATH(cPath) THEOS_PACKAGE_INSTALL_PREFIX cPath
 #define ROOT_PATH_NS(path) @THEOS_PACKAGE_INSTALL_PREFIX path
+#define ROOT_PATH_NS_VAR(path) [@THEOS_PACKAGE_INSTALL_PREFIX stringByAppendingPathComponent:path]
 #endif
