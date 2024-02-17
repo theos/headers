ad2519080c684fc5cadd435a3a52535b692f024b
Lightmann <lightmanns11@gmail.com>
2024-02-16 23:09:31 -0500

    Update rootless.h for libroot

--- rootless.h
+++ rootless.h
@@ -1,24 +1,22 @@
-#include <sys/syslimits.h>
-#include <unistd.h>
+// TODO: remove this file
+// #include <sys/syslimits.h>
+// #include <unistd.h>
+#include "libroot.h"
 
-#ifdef XINA_SUPPORT // Only define this for rootful compilations that need support for xina
-#define ROOT_PATH(cPath) !access("/var/LIY", F_OK) ? "/var/jb" cPath : cPath
-#define ROOT_PATH_NS(path) !access("/var/LIY", F_OK) ? @"/var/jb" path : path
-#define ROOT_PATH_NS_VAR(path) !access("/var/LIY", F_OK) ? [@"/var/jb" stringByAppendingPathComponent:path] : path
-#define ROOT_PATH_VAR(path) !access("/var/LIY", F_OK) ? ({ \
-	char outPath[PATH_MAX]; \
-	strlcpy(outPath, "/var/jb", PATH_MAX); \
-	strlcat(outPath, path, PATH_MAX); \
-	outPath; \
-}) : path
-#else
-#define ROOT_PATH(cPath) THEOS_PACKAGE_INSTALL_PREFIX cPath
-#define ROOT_PATH_NS(path) @THEOS_PACKAGE_INSTALL_PREFIX path
-#define ROOT_PATH_NS_VAR(path) [@THEOS_PACKAGE_INSTALL_PREFIX stringByAppendingPathComponent:path]
-#define ROOT_PATH_VAR(path) sizeof(THEOS_PACKAGE_INSTALL_PREFIX) > 1 ? ({ \
-    char outPath[PATH_MAX]; \
-    strlcpy(outPath, THEOS_PACKAGE_INSTALL_PREFIX, PATH_MAX); \
-	strlcat(outPath, path, PATH_MAX); \
-    outPath; \
-}) : path
+#ifdef XINA_SUPPORT
+_Pragma("message(\"'XINA_SUPPORT' is deprecated. libroot will now handle this for you.\")")
 #endif
+// #define ROOT_PATH(cPath) THEOS_PACKAGE_INSTALL_PREFIX cPath
+// #define ROOT_PATH_VAR(path) sizeof(THEOS_PACKAGE_INSTALL_PREFIX) > 1 ? ({ \
+//     char outPath[PATH_MAX]; \
+//     strlcpy(outPath, THEOS_PACKAGE_INSTALL_PREFIX, PATH_MAX); \
+// 	strlcat(outPath, path, PATH_MAX); \
+//     outPath; \
+// }) : path
+#define ROOT_PATH(cPath) _Pragma("message(\"'ROOT_PATH' is deprecated. Please import libroot.h and use 'JBROOT_PATH_CSTRING' instead.\")") JBROOT_PATH_CSTRING(cPath)
+#define ROOT_PATH_VAR(path) _Pragma("message(\"'ROOT_PATH_VAR' is deprecated. Please import libroot.h and use 'JBROOT_PATH_CSTRING' instead.\")") JBROOT_PATH_CSTRING(path)
+
+// #define ROOT_PATH_NS(path) @THEOS_PACKAGE_INSTALL_PREFIX path
+// #define ROOT_PATH_NS_VAR(path) [@THEOS_PACKAGE_INSTALL_PREFIX stringByAppendingPathComponent:path]
+#define ROOT_PATH_NS(path) _Pragma("message(\"'ROOT_PATH_NS' is deprecated. Please import libroot.h and use 'JBROOT_PATH_NSSTRING' instead.\")") JBROOT_PATH_NSSTRING(path)
+#define ROOT_PATH_NS_VAR(path) _Pragma("message(\"'ROOT_PATH_NS_VAR' is deprecated. Please import libroot.h and use 'JBROOT_PATH_NSSTRING' instead.\")") JBROOT_PATH_NSSTRING(path)
