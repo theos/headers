3ba029c7878e28609f1c0a93a7a8dff868bafe60
Leptos <leptos.0.null@gmail.com>
2024-09-03 13:57:50 -0700

    Add module.modulemap for UserNotificationsUIKit

---
+++ UserNotificationsUIKit/UserNotificationsUIKit.h
@@ -0,0 +1,8 @@
+#import <UserNotificationsUIKit/NCAnimatableBlurringView.h>
+#import <UserNotificationsUIKit/NCMaterialView.h>
+#import <UserNotificationsUIKit/NCNotificationLongLookView.h>
+#import <UserNotificationsUIKit/NCNotificationRequest+Additions.h>
+#import <UserNotificationsUIKit/NCNotificationShortLookView.h>
+#import <UserNotificationsUIKit/NCNotificationStaticContentProviding.h>
+#import <UserNotificationsUIKit/NCNotificationViewController.h>
+#import <UserNotificationsUIKit/NCShortLookView.h>
