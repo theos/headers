4cc834993722d8fabeaf45b7ff30e7b25c86469e
Leptos <leptos.0.null@gmail.com>
2024-09-03 13:57:54 -0700

    Add module.modulemap for BulletinBoard

---
+++ BulletinBoard/BulletinBoard.h
@@ -0,0 +1,17 @@
+#import <BulletinBoard/BBAction.h>
+#import <BulletinBoard/BBAppearance.h>
+#import <BulletinBoard/BBBulletin.h>
+#import <BulletinBoard/BBBulletinRequest.h>
+#import <BulletinBoard/BBDataProvider.h>
+#import <BulletinBoard/BBDataProviderIdentity.h>
+#import <BulletinBoard/BBLocalDataProviderStore.h>
+#import <BulletinBoard/BBSectionIcon.h>
+#import <BulletinBoard/BBSectionIconVariant.h>
+#import <BulletinBoard/BBSectionInfo.h>
+#import <BulletinBoard/BBSectionInfoSettings.h>
+#import <BulletinBoard/BBSectionParameters.h>
+#import <BulletinBoard/BBSectionSubtypeParameters.h>
+#import <BulletinBoard/BBServer.h>
+#import <BulletinBoard/BBSettingsGateway.h>
+#import <BulletinBoard/BBThumbnailSizeConstraints.h>
+#import <BulletinBoard/BBWeeAppController.h>
