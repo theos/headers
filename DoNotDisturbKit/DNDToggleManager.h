869196b23cf5b17c8ffc35d2a09c8ef3b968eac5
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2025-08-13 15:02:08 +0100

    move DNDToggleManager to DoNotDisturbKit, mark as ios 14+

--- DoNotDisturbKit/DNDToggleManager.h
+++ DoNotDisturbKit/DNDToggleManager.h
@@ -1,7 +1,11 @@
 #import <Foundation/Foundation.h>
 
+API_AVAILABLE(ios(14.0))
 @interface DNDToggleManager : NSObject
 
 + (instancetype)managerForClientIdentifier:(NSString *)identifier;
+
+- (BOOL)_toggleDNDOffReturningError:(NSError *)error;
+- (BOOL)_toggleDNDOnReturningError:(NSError *)error;
 
 @end
