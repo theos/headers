cdc9cee151ab5a438303c2b4847503ede4acfa63
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2025-08-15 15:27:59 +0100

    add availability macros to DNDState and DNDStateService

--- DoNotDisturb/DNDState.h
+++ DoNotDisturb/DNDState.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 
+API_AVAILABLE(ios(12.0))
 @interface DNDState : NSObject
 
 - (BOOL)isActive;
--- DoNotDisturb/DNDStateService.h
+++ DoNotDisturb/DNDStateService.h
@@ -1,5 +1,6 @@
 #import "DNDState.h"
 
+API_AVAILABLE(ios(12.0))
 @interface DNDStateService : NSObject
 
 + (instancetype)serviceForClientIdentifier:(NSString *)identifier;
